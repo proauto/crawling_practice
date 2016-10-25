6fb48ac1186a61bed7e5ce56a590705b8ec2f4dd
proauto <hg1771@naver.com>
2016-10-25 19:30:07 +0900

    fix gitignore

---
+++ wikiparse.py
@@ -0,0 +1,45 @@
+#!C:\Python34\python.exe
+import xml.etree.ElementTree as etree
+import urllib.parse, sys
+
+wikifile = "/Users/ihong-gyu/Downloads/kowiki-latest-pages-articles.xml"
+ns = "{http://www.mediawiki.org/xml/export-0.10/}"
+dcount = 0
+dlimit = 10000
+if len(sys.argv) > 1:
+        dlimit = int(sys.argv[1])
+filecount = 1;
+idxfilename = "data/kowiki%d.idx"%filecount
+for event, elem in etree.iterparse(wikifile,events=('start','end')):
+        if event == 'end':
+                if elem.tag == ns+"page":
+                        if dcount == 0:
+                                fidx = open(idxfilename,'w',encoding='UTF-8')
+                                print("\nwriting %s"%idxfilename)
+                        dcount += 1
+                        print("\r%d"%dcount,end="",flush=True)
+                        for first in elem.getchildren():
+                                if first.tag == ns+"title":
+                                        stitle = urllib.parse.quote_plus(first.text)
+                                        fidx.write("#DREREFERENCE http://ko.wikipedia.org/wiki/%s\n"%stitle)
+                                        fidx.write("#DRETITLE %s\n"%first.text)
+                                if first.tag == ns+"id":
+                                        fidx.write("#DREFIELD wiki_id=%s\n"%first.text)
+                                if first.tag == ns+"revision":
+                                        for second in first.getchildren():
+                                                if second.tag == ns+"timestamp":
+                                                        fidx.write("#DREDATE %s\n"%second.text)
+                                                if second.tag == ns+"text":
+                                                        fidx.write("#DRECONTENT\n%s\n"%second.text)
+                        fidx.write("#DREENDDOC\n")
+                        if dcount == dlimit:
+                                filecount += 1
+                                dcount = 0
+                                fidx.write("#DREENDDATANOOP\n")
+                                fidx.close()
+                                idxfilename = "data/kowiki{}.idx".format(filecount)
+                        elem.clear()
+if dcount > 0 and dcount < dlimit:
+	fidx.write("#DREENDDATANOOP\n")
+	fidx.close()
+
---
+++ wikipedia_crawling.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import os
+
+
+def wiki_spider():
+
+    # 다운로드 경로
+    down_url = "https://dumps.wikimedia.org/kowiki/latest/kowiki-latest-pages-articles.xml.bz2"
+
+    # 버전 확인을 위한 경로
+    url = 'https://dumps.wikimedia.org/kowiki/latest/'
+    file = "kowiki-latest-pages-articles.xml.bz2"
+
+    # 크롤링을 통해서 버전이 일치하는지 확인
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'lxml')
+
+    # 과거 버전 존재 확인
+    if(os.path.isfile("data/meta-data.txt")):
+        old_row = open('data/meta-data.txt','r').readline()
+    else:
+        old_row = ""
+
+    # 버전 일치 확인
+    for row in soup.get_text().splitlines():
+        row_sp = row.split(' ',2)
+        if(row_sp[0] == file):
+            # 버전 일치하면 그대로
+            if(row == old_row):
+                print("변경 사항 없음")
+            # 버전이 다르면 위키피디아 데이터 덤프를 가져옴 - 현재는 다운로드 경로 - 훗날 이동시킬 것
+            else:
+                f = open("data/meta-data.txt", 'w')
+                f.write(str(row))
+                f.close()
+                print("새로운 값으로 변경")
+                os.system("python -m webbrowser -t \"" + down_url + "\"")
+
+
+
+wiki_spider()
+
